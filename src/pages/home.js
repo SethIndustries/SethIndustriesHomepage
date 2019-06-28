ac660fcec546a78a79440d36c3d5fdb44812ca30
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-28 22:55:08 +0100

    Spreading bars.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Tween, Timeline } from 'react-gsap'
+
+import '../assets/css/mystyles.css'
 
 class HomePage extends React.Component {
   render() {
     return (
       <Layout location={this.props.location}>
+        <Tween duration={2} from={{ height: "0%" }} to={{ height: "100%" }}>
+          <div className="splitter-rect" />
+        </Tween>
         <h1>My text here</h1>
+        <Tween duration={2} from={{ height: "0%" }} to={{ height: "100%" }}>
+          <div className="splitter-rect" />
+        </Tween>
       </Layout>
     )
   }
