73ecd17da47af55de9c4f9bd5644821f9a95a8d6
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-29 14:33:03 +0100

    Page updates.

--- src/components/menu.js
+++ src/components/menu.js
@@ -14,10 +14,11 @@ const Menu = () => {
   let myElMenuItem3 = useRef(null)
   let myElMenuItem4 = useRef(null)
   let myElMenuItem5 = useRef(null)
+  let myElMenuItem6 = useRef(null)
 
   useEffect(() => {
     new TimelineMax({paused: true})
-      .staggerFromTo([myElMenuItem, myElMenuItem2, myElMenuItem3, myElMenuItem4, myElMenuItem5], 0.25, {y: -20}, {y: 0, opacity: 1}, 0.25)
+      .staggerFromTo([myElMenuItem, myElMenuItem2, myElMenuItem3, myElMenuItem4, myElMenuItem5, myElMenuItem6], 0.25, {y: -20}, {y: 0, opacity: 1}, 0.25)
       .play()
   }, [])
 
@@ -26,11 +27,12 @@ const Menu = () => {
       <ul id="menu">
         <ul>
           <center>
-            <li><Link ref={div => myElMenuItem = div} to="/intro">Intro</Link></li>
-            <li><Link ref={div => myElMenuItem2 = div} to="/portfolio">Portfolio</Link></li>
-            <li><Link ref={div => myElMenuItem3 = div} to="/about">About</Link></li>
-            <li><Link ref={div => myElMenuItem4 = div} to="/blog">Blog</Link></li>
-            <li><Link ref={div => myElMenuItem5 = div} to="/contact">Contact</Link></li>
+            <li><Link ref={div => myElMenuItem = div} to="/home">Home</Link></li>
+            <li><Link ref={div => myElMenuItem2 = div} to="/intro">Intro</Link></li>
+            <li><Link ref={div => myElMenuItem3 = div} to="/portfolio">Portfolio</Link></li>
+            <li><Link ref={div => myElMenuItem4 = div} to="/about">About</Link></li>
+            <li><Link ref={div => myElMenuItem5 = div} to="/blog">Blog</Link></li>
+            <li><Link ref={div => myElMenuItem6 = div} to="/contact">Contact</Link></li>
           </center>
         </ul>
       </ul>
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,13 +2,32 @@ import React from 'react'
 import Menu from '../components/menu'
 
 import '../assets/css/mystyles.css'
+import '../assets/scss/mystyles.scss'
 import './white-background.css'
 
 export default function HomePage() {
   return(
     <div className="white-background">
       <Menu />
-      <h1>Portfolio</h1>
+      <center>
+        <h1>Portfolio</h1>
+        <div className="portfolio-grid">
+          <div className='portfolio-grid-left'>
+            <h1>My content goes here.</h1>
+          </div>
+          <div className='portfolio-grid-right'>
+            <h3>Clients</h3>
+            <ul>
+              <li>KPMG</li>
+              <li>DOOH.com</li>
+              <li>Questagame</li>
+              <li>Jemfusion</li>
+              <li>Wedje Designs</li>
+              <li>Dan & Dan Landscaping</li>
+            </ul>
+          </div>
+        </div>
+      </center>
     </div>
   )
 }
