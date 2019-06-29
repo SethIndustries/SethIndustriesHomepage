89e5995a6a5965339bb0c1a2646ba9e2cc812b01
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-29 10:47:37 +0100

    Rewrite animation to use hooks.

---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TimelineMax } from "gsap/TweenMax";
+
+export default class HomePage extends React.Component {
+  render() {
+    return(
+      <h1>My heading goes here.</h1>
+    )
+  }
+}
--- src/pages/intro.js
+++ src/pages/intro.js
@@ -1,86 +1,81 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { TimelineMax } from "gsap/TweenMax";
 
 import '../assets/css/mystyles.css'
 
-class IntroPage extends React.Component {
-  constructor(props){
-    super(props);
-    // reference to the DOM node
-    this.myElSplitterRect = null;
-    this.myElIcon = null;
-    this.myElTitle = null;
-    this.myElTitle2 = null;
-    // reference to the animation
-    this.myTwTimeline = new TimelineMax({paused: true})
+function IntroPage() {
+  let myElSplitterRect = useRef(null)
+  let myElIcon = useRef(null)
+  let myElTitle = useRef(null)
+  let myElTitle2 = useRef(null)
+  let myTwTimeline = new TimelineMax({paused: true})
 
-    this.state = {
-      title: 'Seth',
-      title2: 'Industries'
-    }
-  }
+  const [splitterRect, setSplitterRect] = useState(null);
+  const [title, setTitle] = useState('Seth');
+  const [title2, setTitle2] = useState('Industries');
 
-  componentDidMount(){
-    // use the node ref to create the animation
-    this.myTwTimeline
-      .fromTo(this.myElSplitterRect, 1, {height: "0px"}, {height: "1000px"})
-      .fromTo(this.myElSplitterRect, 1, {width: "0.3%"}, {width: "100%"})
-      .fromTo(this.myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
-      .staggerFromTo([
-          this.myElTitle,
-          this.myElTitle2
-        ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
-      )
-      .staggerFromTo([
-          this.myElIcon,
-          this.myElTitle,
-          this.myElTitle2
-        ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-      )
-      .add(() => { this.setState({ title: 'Custom', title2: 'Software' }) })
-      .staggerFromTo([
-          this.myElTitle,
-          this.myElTitle2
-        ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
-      )
-      .staggerFromTo([
-          this.myElIcon,
-          this.myElTitle,
-          this.myElTitle2
-        ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-      )
-      .add(() => { this.setState({ title: 'You ask.', title2: 'We deliver.' }) })
-      .fromTo(this.myElTitle, 1, {y: -20, opacity: 0}, {y: 0, opacity: 1})
-      .fromTo(this.myElTitle2, 3, {y: -60, opacity: 0}, {y: 0, opacity: 1})
-      .staggerFromTo([
-          this.myElTitle,
-          this.myElTitle2
-        ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-      )
-      .add(() => { window.location.replace('/home') })
-      .play()
-  }
+  useEffect(() => {
+    setSplitterRect(
+      // use the node ref to create the animation
+      myTwTimeline
+        .fromTo(myElSplitterRect, 1, {height: "0px"}, {height: "1000px"})
+        .fromTo(myElSplitterRect, 1, {width: "0.3%"}, {width: "100%"})
+        .fromTo(myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
+        .staggerFromTo([
+            myElTitle,
+            myElTitle2
+          ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
+        )
+        .staggerFromTo([
+            myElIcon,
+            myElTitle,
+            myElTitle2
+          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
+        )
+        .add(() => { setTitle('Custom'); setTitle2('Software') })
+        .staggerFromTo([
+            myElTitle,
+            myElTitle2
+          ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
+        )
+        .staggerFromTo([
+            myElIcon,
+            myElTitle,
+            myElTitle2
+          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
+        )
+        .add(() => { setTitle('You ask.'); setTitle2('We deliver.') })
+        .fromTo(myElTitle, 1, {opacity: 0}, {opacity: 1})
+        .fromTo(myElTitle2, 3, {opacity: 0}, {opacity: 1})
+        .staggerFromTo([
+            myElTitle,
+            myElTitle2
+          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
+        )
+        .add(() => { window.location.replace('/home') })
+        .pause()
+    );
+    myTwTimeline.play()
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <div className="splitter-rect" ref={div => this.myElSplitterRect = div}>
-          <div className="inner-content">
-            <img
-              ref={div => this.myElIcon = div}
-              className="icon"
-              alt=''
-              src={require('./../images/icon.svg')}
-              />
-              <div className="title-content">
-                <h1 className="title" ref={div => this.myElTitle = div}>{this.state.title}</h1>
-                <h1 className="title" ref={div => this.myElTitle2 = div}>{this.state.title2}</h1>
-              </div>
-          </div>
+  return (
+    <div>
+      <div className="splitter-rect" ref={div => myElSplitterRect = div}>
+        <div className="inner-content">
+          <img
+            ref={div => myElIcon = div}
+            className="icon"
+            alt=''
+            src={require('./../images/icon.svg')}
+            />
+            <div className="title-content">
+              <h1 className="title" ref={div => myElTitle = div}>{title}</h1>
+              <h1 className="title" ref={div => myElTitle2 = div}>{title2}</h1>
+            </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default IntroPage
