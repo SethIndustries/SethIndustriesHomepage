9698f96a4e93b10378cd777955a0b9607273dd87
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-29 11:30:36 +0100

    Fix up backgrounds.

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { TimelineMax } from "gsap/TweenMax";
 
-export default class HomePage extends React.Component {
-  render() {
-    return(
-      <h1>My heading goes here.</h1>
-    )
-  }
+import '../assets/css/mystyles.css'
+import './white-background.css'
+
+export default function HomePage() {
+  let myElSplitterRect = useRef(null)
+  let myElIcon = useRef(null)
+  let myElTitle = useRef(null)
+  let myElTitle2 = useRef(null)
+  let myTwTimeline = new TimelineMax({paused: true})
+
+  useEffect(() => {
+    myTwTimeline
+      .fromTo(myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
+      .fromTo([myElTitle, myElTitle2], 1, {y: -20}, {y: 0, opacity: 1})
+    myTwTimeline.play()
+  }, [])
+
+  return(
+    <div className="white-background">
+      <div className="inner-content">
+        <img
+          ref={div => myElIcon = div}
+          className="icon"
+          alt=''
+          src={require('./../images/icon.svg')}
+        />
+        <div className="title-content">
+          <h1 className="title" ref={div => myElTitle = div}>Seth</h1>
+          <h1 className="title" ref={div => myElTitle2 = div}>Industries</h1>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/intro.js
+++ src/pages/intro.js
@@ -2,83 +2,74 @@ import React, { useState, useRef, useEffect } from 'react'
 import { TimelineMax } from "gsap/TweenMax";
 
 import '../assets/css/mystyles.css'
+import './black-background.css'
 
-function IntroPage() {
+export default function IntroPage() {
   let myElSplitterRect = useRef(null)
   let myElIcon = useRef(null)
   let myElTitle = useRef(null)
   let myElTitle2 = useRef(null)
   let myTwTimeline = new TimelineMax({paused: true})
 
-  const [splitterRect, setSplitterRect] = useState(null);
   const [title, setTitle] = useState('Seth');
   const [title2, setTitle2] = useState('Industries');
 
   useEffect(() => {
-    setSplitterRect(
-      // use the node ref to create the animation
-      myTwTimeline
-        .fromTo(myElSplitterRect, 1, {height: "0px"}, {height: "1000px"})
-        .fromTo(myElSplitterRect, 1, {width: "0.3%"}, {width: "100%"})
-        .fromTo(myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
-        .staggerFromTo([
-            myElTitle,
-            myElTitle2
-          ], 1, {y: -20}, {y: 0, opacity: 1}, 0.5
-        )
-        .staggerFromTo([
-            myElIcon,
-            myElTitle,
-            myElTitle2
-          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-        )
-        .to(myElIcon, 0, { display: 'none' })
-        .add(() => { setTitle('Custom'); setTitle2('Software') })
-        .staggerFromTo([
-            myElTitle,
-            myElTitle2
-          ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
-        )
-        .staggerFromTo([
-            myElIcon,
-            myElTitle,
-            myElTitle2
-          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-        )
-        .add(() => { setTitle('You ask.'); setTitle2('We deliver.') })
-        .fromTo(myElTitle, 1, {opacity: 0}, {opacity: 1})
-        .to(myElIcon, 1, {display: 'none'}) // Pause for dramatic effect
-        .fromTo(myElTitle2, 3, {opacity: 0}, {opacity: 1})
-        .staggerFromTo([
-            myElTitle,
-            myElTitle2
-          ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
-        )
-        .to(myElSplitterRect, 1, {width: "0.3%"})
-        .add(() => { window.location.replace('/home') })
-        .pause()
-    );
+    myTwTimeline
+      .fromTo(myElSplitterRect, 1, {height: "0px"}, {height: "1000px"})
+      .fromTo(myElSplitterRect, 1, {width: "0.3%"}, {width: "100%"})
+      .fromTo(myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
+      .staggerFromTo([
+          myElTitle,
+          myElTitle2
+        ], 1, {y: -20}, {y: 0, opacity: 1}, 0.5
+      )
+      .staggerFromTo([
+          myElIcon,
+          myElTitle,
+          myElTitle2
+        ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
+      )
+      .to(myElIcon, 0, { display: 'none' })
+      .add(() => { setTitle('Custom'); setTitle2('Software') })
+      .staggerFromTo([
+          myElTitle,
+          myElTitle2
+        ], 1, {y: -20, opacity: 0}, {y: 0, opacity: 1}, 0.5
+      )
+      .staggerFromTo([
+          myElIcon,
+          myElTitle,
+          myElTitle2
+        ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
+      )
+      .add(() => { setTitle('You ask.'); setTitle2('We deliver.') })
+      .fromTo(myElTitle, 1, {opacity: 0}, {opacity: 1})
+      .to(myElIcon, 1, {display: 'none'}) // Pause for dramatic effect
+      .fromTo(myElTitle2, 3, {opacity: 0}, {opacity: 1})
+      .staggerTo([
+          myElTitle,
+          myElTitle2
+        ], 1, {opacity: 0}, 0.5
+      )
+      .add(() => { window.location.replace('/home') })
     myTwTimeline.play()
   }, [])
 
   return (
-    <div>
-      <div className="splitter-rect" ref={div => myElSplitterRect = div}>
-        <div className="inner-content">
-          <img
-            ref={div => myElIcon = div}
-            className="icon"
-            alt=''
-            src={require('./../images/icon.svg')}
-          />
-          <div className="title-content">
-            <h1 className="title" ref={div => myElTitle = div}>{title}</h1>
-            <h1 className="title" ref={div => myElTitle2 = div}>{title2}</h1>
-          </div>
+    <div className="splitter-rect" ref={div => myElSplitterRect = div}>
+      <div className="inner-content">
+        <img
+          ref={div => myElIcon = div}
+          className="icon"
+          alt=''
+          src={require('./../images/icon.svg')}
+        />
+        <div className="title-content">
+          <h1 className="title" ref={div => myElTitle = div}>{title}</h1>
+          <h1 className="title" ref={div => myElTitle2 = div}>{title2}</h1>
         </div>
       </div>
     </div>
   )
 }
-
-export default IntroPage
