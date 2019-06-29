801ff50873f8dc338b3d09626f5ffefbef9dcdef
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-29 14:08:04 +0100

    Update css.

--- src/pages/intro.js
+++ src/pages/intro.js
@@ -16,7 +16,7 @@ export default function IntroPage() {
 
   useEffect(() => {
     myTwTimeline
-      .fromTo(myElSplitterRect, 1, {height: "0px"}, {height: "1000px"})
+      .fromTo(myElSplitterRect, 1, {height: "0.3%"}, {height: "100%"})
       .fromTo(myElSplitterRect, 1, {width: "0.3%"}, {width: "100%"})
       .fromTo(myElIcon, 1.3, {x: 60, opacity: 0}, {x: 0, opacity: 1})
       .staggerFromTo([
@@ -43,7 +43,8 @@ export default function IntroPage() {
           myElTitle2
         ], 1, {y: 0}, {y: 20, opacity: 0}, 0.5
       )
-      .add(() => { setTitle('You ask.'); setTitle2('We deliver.') })
+      .add(() => { setTitle('You describe.'); setTitle2('We deliver.') })
+      .pause()
       .fromTo(myElTitle, 1, {opacity: 0}, {opacity: 1})
       .to(myElIcon, 1, {display: 'none'}) // Pause for dramatic effect
       .fromTo(myElTitle2, 3, {opacity: 0}, {opacity: 1})
@@ -57,17 +58,19 @@ export default function IntroPage() {
   }, [])
 
   return (
-    <div className="splitter-rect" ref={div => myElSplitterRect = div}>
-      <div className="inner-content">
-        <img
-          ref={div => myElIcon = div}
-          className="icon"
-          alt=''
-          src={require('./../images/icon.svg')}
-        />
-        <div className="title-content">
-          <h1 className="title" ref={div => myElTitle = div}>{title}</h1>
-          <h1 className="title" ref={div => myElTitle2 = div}>{title2}</h1>
+    <div className="black-background">
+      <div className="splitter-rect" ref={div => myElSplitterRect = div}>
+        <div className="inner-content">
+          <img
+            ref={div => myElIcon = div}
+            className="icon"
+            alt=''
+            src={require('./../images/icon.svg')}
+          />
+          <div className="title-content">
+            <h1 className="title" ref={div => myElTitle = div}>{title}</h1>
+            <h1 className="title" ref={div => myElTitle2 = div}>{title2}</h1>
+          </div>
         </div>
       </div>
     </div>
