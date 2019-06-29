95a851c8e8d50d3dd820212e2fd8cf9bf4dbdbea
JeremyEllingham <jeremy@sethindustries.com.au>
2019-06-29 14:46:12 +0100

    Update portfolio page.

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -13,9 +13,6 @@ export default function HomePage() {
         <h1>Portfolio</h1>
         <div className="portfolio-grid">
           <div className='portfolio-grid-left'>
-            <h1>My content goes here.</h1>
-          </div>
-          <div className='portfolio-grid-right'>
             <h3>Clients</h3>
             <ul>
               <li>KPMG</li>
@@ -24,6 +21,19 @@ export default function HomePage() {
               <li>Jemfusion</li>
               <li>Wedje Designs</li>
               <li>Dan & Dan Landscaping</li>
+            </ul>
+          </div>
+          <div className='portfolio-grid-middle'>
+            <h1>My content goes here.</h1>
+          </div>
+          <div className='portfolio-grid-right'>
+            <h2>KPMG</h2>
+            <ul>
+              <li>Industry: Accounting</li>
+              <li>Location: React</li>
+              <li>Website: <u><a href="https://google.com" target="_blank">kpmg.com.au</a></u></li>
+              <li>Tech: React</li>
+              <li>Description: We did such and such and such.</li>
             </ul>
           </div>
         </div>
